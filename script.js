68c133961eb24045f852f0dfbc88ce74cd81b65e
Pankaj44851 <125351108+Pankaj44851@users.noreply.github.com>
2024-02-26 15:47:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// testimonail crousel js
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,1],
+        pagination:true,
+        autoPlay:false
+    });
+});
